27481e3a2b9824bad914a4ee946f050806e11584
Toby Inkster <mail@tobyinkster.co.uk>
2013-03-19 23:10:34 +0000

    docs

--- lib/Module/Install/Admin/Credits.pm
+++ lib/Module/Install/Admin/Credits.pm
@@ -125,3 +125,45 @@ sub _people
 
 1;
 
+__END__
+
+=head1 NAME
+
+Module::Install::Admin::Credits - author-side part of Module::Install::Credits
+
+=head1 DESCRIPTION
+
+Not really documented much right now.
+
+=begin private
+
+=item write_credits_file
+
+=end private
+
+=head1 BUGS
+
+Please report any bugs to
+L<http://rt.cpan.org/Dist/Display.html?Queue=Module-Install-Copyright>.
+
+=head1 SEE ALSO
+
+L<Module::Install::Credits>.
+
+=head1 AUTHOR
+
+Toby Inkster E<lt>tobyink@cpan.orgE<gt>.
+
+=head1 COPYRIGHT AND LICENCE
+
+This software is copyright (c) 2013 by Toby Inkster.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=head1 DISCLAIMER OF WARRANTIES
+
+THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+
